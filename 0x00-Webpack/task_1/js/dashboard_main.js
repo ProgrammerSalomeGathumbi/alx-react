7778e3fffc213655626df1dff87fb18e79fd9c44
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-08-21 20:51:21 +0300

    changed order of items

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -7,10 +7,10 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-  let count = 0;
-  const updateCounter = () => {
-    count++;
-    $('#count').html(`${count} clicks on the button`);
-  };
+let count = 0;
+const updateCounter = () => {
+   count++;
+   $('#count').html(`${count} clicks on the button`);
+};
 
-  $('#clickButton').on('click', debounce(updateCounter, 300));
+$('#clickButton').on('click', debounce(updateCounter, 500));
