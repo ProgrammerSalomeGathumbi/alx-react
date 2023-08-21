42a60413e346f12bdd996b3fe8cc25276572be98
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-08-21 15:30:08 +0300

    fixed syntax error

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import debounce from 'lodash/debounce';
 
 $('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the studentsClick here to get started</p>');
+$('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
