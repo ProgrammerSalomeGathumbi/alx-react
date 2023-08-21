88fc15eff3a2acb5e0498bcaa10c084cfc62e64f
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-08-21 15:36:14 +0300

    Added missing section

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -4,7 +4,8 @@ module.exports = {
 entry: './js/dashboard_main.js',
 output: {
   filename: 'bundle.js',
-  path: path.resolve(__dirname, 'public')
+  path: path.resolve(__dirname, 'public'),
+  publicPath: '/public'
 },
   mode: 'production',	
 };
