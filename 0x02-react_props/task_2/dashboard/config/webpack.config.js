d6861fc1d7e2c1d7c11df6ba2c40ad57a09bc242
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-08-28 09:29:53 +0300

    added task 2

---
+++ 0x02-react_props/task_2/dashboard/config/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+     entry: './src/index.js',
+ 
+  output: {
+        filename: 'bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+	use: ['style-loader', 'css-loader'],       
+      },
+      {
+        test: /\.(gif|svg|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        use: [
+	  {
+	    loader: 'file-loader', 
+            options: {
+	            byPassOnDebug: true,
+                    disable: true, 				   
+	    },		    
+	},
+        'image-webpack-loader',
+	],	
+      },
+      {
+        test: /\.(js|jsx)$/,
+	exclude: /node_modules/,
+        use: ["babel-loader"],	
+      },   
+    ],	
+	},
+resolve: {
+   extensions: [".*", ".js", ".jsx"],	 
+	 },
+  plugins: [
+    new HtmlWebpackPlugin({
+      name: "index.html",
+      inject:false,      
+      template: './dist/index.html',
+    }),
+    ],
+  devServer: {
+    static: './dist',	  
+    port: 8564, 
+    open: true,
+    compress: true, 
+    hot: true,
+  },
+  devtool: 'inline-source-map',   
+};
