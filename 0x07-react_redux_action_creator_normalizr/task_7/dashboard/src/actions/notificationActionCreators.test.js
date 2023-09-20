04b5407b8c1f0327911246862b532ac553f0a937
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-09-20 12:13:37 +0300

    task 7:Async Action Creators

---
+++ 0x07-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/notificationActionCreators.test.js
@@ -0,0 +1,24 @@
+import { markAsRead, setNotificationFilter } from '../notificationActionCreators';
+import { NotificationTypeFilters, MARK_AS_READ, SET_TYPE_FILTER } from '../notificationActionTypes';
+
+describe('markAsRead action creator', () => {
+  it('should create an action to mark a notification as read', () => {
+    const index = 1;
+    const expectedAction = {
+      type: MARK_AS_READ,
+      index,
+    };
+    expect(markAsRead(index)).toEqual(expectedAction);
+  });
+});
+
+describe('setNotificationFilter action creator', () => {
+  it('should create an action to set the notification filter', () => {
+    const filter = 'DEFAULT';
+    const expectedAction = {
+      type: SET_TYPE_FILTER,
+      filter,
+    };
+    expect(setNotificationFilter(filter)).toEqual(expectedAction);
+  });
+})
