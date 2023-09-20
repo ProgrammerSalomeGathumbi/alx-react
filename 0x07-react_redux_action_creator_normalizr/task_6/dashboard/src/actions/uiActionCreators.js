399728e37617a11d49ce108b537f52af04af2d1a
programmersalomegathumbi <salomegathumbi@gmail.com>
2023-09-20 10:34:06 +0300

    task 6:Bound the actions

---
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,44 @@
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from './uiActionTypes';
+
+export function login(email, password) {
+  return {
+    type: LOGIN,
+    user: {
+      email,
+      password,
+    },
+  };
+}
+
+export function logout() {
+  return {
+    type: LOGOUT,
+  };
+}
+
+export function displayNotificationDrawer() {
+  return {
+    type: DISPLAY_NOTIFICATION_DRAWER,
+  };
+}
+
+export function hideNotificationDrawer() {
+  return {
+    type: HIDE_NOTIFICATION_DRAWER,
+  };
+}
+export const boundLogin = (email, password) => (dispatch) => {
+  dispatch(login(email, password));
+};
+
+export const boundLogout = () => (dispatch) => {
+  dispatch(logout());
+};
+
+export const boundDisplayNotificationDrawer = () => (dispatch) => {
+  dispatch(displayNotificationDrawer());
+};
+
+export const boundHideNotificationDrawer = () => (dispatch) => {
+  dispatch(hideNotificationDrawer());
+};
